a8925e8a84fa69f50fd4bd48973e633f8b829b27
Sachin Atulkar <sachinatulkar00@gmail.com>
2026-07-01 20:32:22 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "project_name" {
+  description = "Project Name"
+  value       = var.project_name
+}
+
+output "aws_region" {
+  description = "AWS Region"
+  value       = var.aws_region
+}
