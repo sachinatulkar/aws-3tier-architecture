a41c0bf893bccd715b1b558f3c351dddf03b0636
Sachin Atulkar <sachinatulkar00@gmail.com>
2026-07-01 20:46:01 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,31 @@
 variable "aws_region" {
-  description = "AWS region where resources will be created"
-  type        = string
-  default     = "ap-south-1"
+  default = "ap-south-1"
 }
 
-variable "project_name" {
-  description = "Project name"
-  type        = string
-  default     = "aws-3tier"
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }
 
-variable "environment" {
-  description = "Deployment environment"
-  type        = string
-  default     = "production"
+variable "public_subnet_1" {
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2" {
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1" {
+  default = "10.0.3.0/24"
+}
+
+variable "private_subnet_2" {
+  default = "10.0.4.0/24"
+}
+
+variable "availability_zone_1" {
+  default = "ap-south-1a"
+}
+
+variable "availability_zone_2" {
+  default = "ap-south-1b"
 }
